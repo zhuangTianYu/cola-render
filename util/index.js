71a116b64ce995acd4fb9bc80ac10cf373541fc9
zhuangtianyu <zhuangtianyu@Koolearn-inc.com>
2019-12-27 12:11:59 +0800

    feat: windowScrollTo

--- util/index.js
+++ util/index.js
@@ -7,6 +7,23 @@ const create = (tagName, classList, innerHTML) => {
   return element
 }
 
+const windowScrollTo = (targetTop, duration = 100) => {
+  console.time('scroll')
+  const startTop = window.scrollY
+  const step = startTop > targetTop
+    ? -Math.ceil((startTop - targetTop) / duration)
+    : -Math.floor((startTop - targetTop) / duration)
+  const interval = setInterval(() => {
+    const currentTop = window.scrollY
+    if (Math.abs(currentTop - targetTop) <= Math.abs(step)) {
+      window.scrollTo(0, targetTop)
+      console.timeEnd('scroll')
+      clearInterval(interval)
+    } else {
+      window.scrollTo(0, currentTop + step)
+    }
+  }, 1)
+}
 
 
 
